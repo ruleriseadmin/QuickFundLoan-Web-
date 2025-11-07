ac27f0ff1f67b43fb37a5d7d49e19f9c3d61c693
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2025-11-07 07:25:45 +0100

    corrected int to float

--- components/repayment/LoanSchedule.tsx
+++ components/repayment/LoanSchedule.tsx
@@ -84,7 +84,10 @@ useEffect(() => {
       {/* Amount and Date section */}
       <div className={`${loan.status === 'completed' ? 'text-[#5A5A5A]' : 'text-[#282828] '} text-[16px]  leading-7 flex  flex-col`}>
         <div className='flex flex-col justify-end items-end'>
-        <span className='font-normal'>{loanHistory[0]?.status === 'OVERDUE' ? formatCurrency(loan.remaining_balance + loanHistory[0]?.penalty_remaining) : formatCurrency(loan?.payment_amount)}</span>
+         <span className='font-normal'>{
+         loanHistory[0]?.has_upfront_interest ?
+        loanHistory[0]?.status === 'OVERDUE' ? formatCurrency(loan.remaining_balance + loanHistory[0]?.penalty_remaining) : formatCurrency(loan?.principal_payment)
+        : loanHistory[0]?.status === 'OVERDUE' ? formatCurrency(loan.remaining_balance + loanHistory[0]?.penalty_remaining) : formatCurrency(loan?.payment_amount)}</span>
         
         {loanHistory[0]?.status !== 'OVERDUE' && loan.status === 'partially_paid' && (
           <span className='font-thin text-[13px]  '><span className='font-bold text-[#1F96A9]'>paid</span> {formatCurrency(loan.payment_amount - loan.remaining_balance)}</span>
